6a8bf626829879d12e8fe1e73de3b0d601632565
rupkumar.radhakrishnana@gmail.com <rupkumar.radhakrishnan@gmail.com>
2016-04-04 14:45:18 -0700

    mongoDB connection

--- src/com/videomanager/model/VideoList.java
+++ src/com/videomanager/model/VideoList.java
@@ -1,5 +1,85 @@
 package com.videomanager.model;
 
+import java.sql.Date;
+
 public class VideoList {
 
+	private String userName;
+	
+	private String fileName;
+	
+	private Date lastUpdated;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((fileName == null) ? 0 : fileName.hashCode());
+		result = prime * result
+				+ ((lastUpdated == null) ? 0 : lastUpdated.hashCode());
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VideoList other = (VideoList) obj;
+		if (fileName == null) {
+			if (other.fileName != null)
+				return false;
+		} else if (!fileName.equals(other.fileName))
+			return false;
+		if (lastUpdated == null) {
+			if (other.lastUpdated != null)
+				return false;
+		} else if (!lastUpdated.equals(other.lastUpdated))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "VideoList [userName=" + userName + ", fileName=" + fileName
+				+ ", lastUpdated=" + lastUpdated + "]";
+	}
+	
+	
+	
 }
